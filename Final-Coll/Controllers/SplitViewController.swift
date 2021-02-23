3c77a259062f80fd5756748f57c6131b5f8ff097
ecenurozsy <ecenur.ozsoy@gmail.com>
2021-02-23 23:49:54 +0300

    iPad ilk açılıştaki arka plan değiştirildi.

--- Final-Coll/Controllers/SplitViewController.swift
+++ Final-Coll/Controllers/SplitViewController.swift
@@ -8,11 +8,16 @@
 import UIKit
 
 class SplitViewController: UISplitViewController {
-
+    
+    let width = UIScreen.main.bounds.width
+    let heigth = UIScreen.main.bounds.height
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        preferredDisplayMode = .allVisible
-                
+        self.preferredDisplayMode = .allVisible;
+        self.maximumPrimaryColumnWidth = width/2;
+        self.minimumPrimaryColumnWidth = heigth/2;
+        self.view.backgroundColor = UIColor.white
     }
 
 }
